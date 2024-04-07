8ac0fd9310a8459e7e608ad1a16b5ee882e04311
Arpit Khandelwal <68700864+Arpit-Khandelwal@users.noreply.github.com>
2024-04-07 17:56:01 +0530

    add components

---
+++ src/Components/WorkSpaceComponents/SetTransaction.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const SetTransaction= () => {
+    return (<div>
+        SetTransaction.
+    </div>)
+}
+export default SetTransaction
---
+++ src/Components/WorkSpaceComponents/StartAuction.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const StartAuction = () => {
+    return (<div>
+        StartAuction..
+    </div>)
+}
+export default StartAuction;
